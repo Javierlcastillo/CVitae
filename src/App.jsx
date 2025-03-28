d866146f2f7024ae4485c81d417ed2fc72105fc8
Javier <jazer2228@gmail.com>
2025-03-28 13:58:33 -0600

    Header no es responsivo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h2>Inicio</h2>
+        <p>Bienvenido a mi portafolio</p>
+      </main>
+    </div>
+  )
+}
+export default App
